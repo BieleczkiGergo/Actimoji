88ba37b16ff74d6f50fd8c8069e4f335f599699d
Bieleczki Gergő <bgergo8b@gmail.com>
2025-01-13 12:48:20 +0100

    started backend

---
+++ backend/Actimoji/src/main/java/hu/actimoji/ActimojiApplication.java
@@ -0,0 +1,13 @@
+package hu.actimoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActimojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActimojiApplication.class, args);
+    }
+
+}
---
+++ backend/Actimoji/src/test/java/hu/actimoji/ActimojiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.actimoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActimojiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
