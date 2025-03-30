4eb95dffad301d1c08e8e1c4d8bb45181dad0b3c
Kocsis Ákos <senseiakos@gmail.com>
2025-03-30 10:55:08 +0200

    commit

--- backend/Actimoji/src/main/java/hu/actimoji/security/LoginResponse.java
+++ backend/Actimoji/src/main/java/hu/actimoji/security/LoginResponse.java
@@ -6,12 +6,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class LoginResponse {
     private String token;
     private String message;
 
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String token, String message) {
+        this.token = token;
+        this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
