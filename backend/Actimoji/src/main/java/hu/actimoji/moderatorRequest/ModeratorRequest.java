70ca7c618f05ed32b396c543f9dca630543a1dc7
Molnár Dávid <david555667@gmail.com>
2025-04-09 17:12:53 +0200

    Become mod

--- backend/Actimoji/src/main/java/hu/actimoji/moderatorRequest/ModeratorRequest.java
+++ backend/Actimoji/src/main/java/hu/actimoji/moderatorRequest/ModeratorRequest.java
@@ -1,5 +1,6 @@
 package hu.actimoji.moderatorRequest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import hu.actimoji.account.Account;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -27,6 +28,7 @@ public class ModeratorRequest {
 
     @ManyToOne
     @JoinColumn(name = "requested_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Account requested;
 
     @ManyToOne
